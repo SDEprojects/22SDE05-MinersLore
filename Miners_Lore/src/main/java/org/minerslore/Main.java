10251400383f8553e22cd00ad0e859d61c94056b
KitaGaither <lakitgai@amazon.com>
2022-09-02 15:00:55 -0700

    Added yaml file, AB#208

--- Miners_Lore/src/main/java/org/minerslore/Main.java
+++ Miners_Lore/src/main/java/org/minerslore/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-//        Story.mainStory();
+//       Story.mainStory();
 
 
         Map map = new Map();
