352ad41a1d119b46d6f8b294f23270f21d782fee
Jamesnorthup <63082279+Jamesnorthup@users.noreply.github.com>
2022-09-02 15:12:10 -0700

    Added Lakita Story Yaml file.

--- Miners_Lore/src/main/java/org/minerslore/Actors/Actor.java
+++ Miners_Lore/src/main/java/org/minerslore/Actors/Actor.java
@@ -5,24 +5,18 @@ import java.awt.Point;
 public class Actor extends Interact_Objects {
     public char symbol;
     public Point Position;
+
+    public Interact_Objects current_Interact_Object;
     int goldKG;
 
-    char N='N';
-    char S='N';
-    char E='N';
-    char W='N';
+
 
     public Actor(char symbol, Point location) {
         super(symbol, location);
         Position=location;
     }
 
-    public void resetNeighbors(){
-        this.N='N';
-        this.S='N';
-        this.E='N';
-        this.W='N';
-    }
+
 
 
 
