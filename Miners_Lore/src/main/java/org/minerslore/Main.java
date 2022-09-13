b36c312aba140bda96c9a46abb5ff4aaa5d2ee45
Jamesnorthup <63082279+Jamesnorthup@users.noreply.github.com>
2022-09-13 10:55:17 -0700

    Commit: Changed monster movement to Taxi cab metric.

--- Miners_Lore/src/main/java/org/minerslore/Main.java
+++ Miners_Lore/src/main/java/org/minerslore/Main.java
@@ -2,26 +2,19 @@ package org.minerslore;
 
 import org.minerslore.stories.Story;
 
-import java.io.IOException;
 import java.util.Scanner;
-import java.lang.Thread;
 
 
-public class Main  { ;
+public class Main {
 
     private static final Scanner reader = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) throws Exception  {
-
-
-//        Story.BeginStory();
+        Story.beginStory();
 
         actionMap.setMapVar();
 
-
-
     }
-
 
 
 }
